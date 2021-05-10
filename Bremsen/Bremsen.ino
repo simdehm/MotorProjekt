77cd37eb230d3fe691239be39c8212f315cebffa
Simon Dehmel <simon.dehmel@fkggoettingen.de>
2021-05-10 09:18:30 +0200

    update

--- Bremsen/Bremsen.ino
+++ Bremsen/Bremsen.ino
@@ -1,6 +1,6 @@
 #include <math.h>
 
-int motor = 1;
+int wmotor = 1;
 int incomingByte = 0;
 int zielGeschwMotor1 = 0;
 int zielGeschwMotor2 = 0;
@@ -10,9 +10,11 @@ int startGeschwMotor1 = 0;
 int startGeschwMotor2 = 0;
 int x1 = 0;
 int x2 = 0;
-int xMax = 1000000;
-int motor1Pin = 10;
-int motor2Pin = 12;
+int xMax = 100000;
+int motor1Pin = 5;
+int motor2Pin = 14;
+int motor1DirPin = 4;
+int motor2DirPin = 12;
 int interruptPin = 2;
 
 void setup() {
@@ -20,31 +22,33 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(interruptPin), interrupt, RISING);
   pinMode(motor1Pin, OUTPUT);
   pinMode(motor2Pin, OUTPUT);
+  pinMode(motor1DirPin, OUTPUT);
+  pinMode(motor2DirPin, OUTPUT);
 }
 
 void loop() {
 
   if (Serial.available() == 0 ) {
     incomingByte  = Serial.read();
-    if (motor == 1) {
+    if (wmotor == 1) {
       zielGeschwMotor1 = incomingByte * 4;
       Serial.write(zielGeschwMotor1);
       startGeschwMotor1 = momentanGeschwMotor1;
       x1 = 0;
-      motor = 2;
+      wmotor = 2;
     } else {
       zielGeschwMotor2 = incomingByte * 4;
       Serial.write(zielGeschwMotor2);
       startGeschwMotor2 = momentanGeschwMotor2;
       x2 = 0;
-      motor = 1;
+      wmotor = 1;
     }
   }
   if (momentanGeschwMotor1 != zielGeschwMotor1 ) {
-   // geschwMotor1();
+    geschwMotor1();
   }
   if (momentanGeschwMotor2 != zielGeschwMotor2) {
-   // geschwMotor2();
+    geschwMotor2();
   }
 }
 
@@ -55,7 +59,13 @@ void geschwMotor1() {
   else {
     momentanGeschwMotor1 = funktionA(startGeschwMotor1, zielGeschwMotor1, 1, x1);
   }
-  analogWrite(motor1Pin, momentanGeschwMotor1);
+  if (momentanGeschwMotor1 < 0) {
+    digitalWrite(motor2DirPin, LOW);
+  }
+  else {
+    digitalWrite(motor2DirPin, HIGH);
+  }
+  analogWrite(motor1Pin, abs(momentanGeschwMotor1));
 }
 
 void geschwMotor2() {
@@ -65,7 +75,13 @@ void geschwMotor2() {
   else {
     momentanGeschwMotor2 = funktionA(startGeschwMotor2, zielGeschwMotor2, 2, x2);
   }
-  analogWrite(motor2Pin, momentanGeschwMotor2);
+  if (momentanGeschwMotor2 < 0) {
+    digitalWrite(motor2DirPin, LOW);
+  }
+  else {
+    digitalWrite(motor2DirPin, HIGH);
+  }
+  analogWrite(motor2Pin, abs(momentanGeschwMotor2));
 }
 
 void interrupt () {
@@ -121,9 +137,9 @@ int funktionC (int startGeschw, int zielGeschw, int motor, int x) {
 int funktionD (int startGeschw, int zielGeschw, int motor, int x) {
   int endGeschw;
   int f0 = 1
-  int g = zielGeschw+1-startGeschw+1;
-  int m = (0-log(((g / (zielGeschw-startGeschw+1)) - 1) / ((g / f0) - 1))) / (g * xMax);
-  endGeschw = g / (1 + 3 ^ ((0 - m) * g * x) * (g / f0 - 1)) + startGeschw-1;
+           int g = zielGeschw + 1 - startGeschw + 1;
+  int m = (0 - log(((g / (zielGeschw - startGeschw + 1)) - 1) / ((g / f0) - 1))) / (g * xMax);
+  endGeschw = g / (1 + 3 ^ ((0 - m) * g * x) * (g / f0 - 1)) + startGeschw - 1;
   if (motor = 1) {
     x1 = x1 + 1;
   }
