185acf91876b5cdc6962f5604d7420bdae7c6f58
simdehm <83570863+simdehm@users.noreply.github.com>
2021-06-10 21:24:32 +0200

    Add files via upload

--- Bremsen/Bremsen.ino
+++ Bremsen/Bremsen.ino
@@ -4,52 +4,57 @@
 #define MOTOR_1_PIN 14
 #define MOTOR_DIR_0_PIN 4
 #define MOTOR_DIR_1_PIN 12
-#define INTERRUPT_PIN 2
-#define INTERRUPT_MANUELL_PIN 10
+/* #define INTERRUPT_PIN 2
+  #define INTERRUPT_MANUELL_PIN 10
 
-#define SENSOR_LINKS_VORNE_PIN 3
-#define SENSOR_LINKS_HINTEN_PIN 4
-#define SENSOR_RECHTS_VORNE_PIN 5
-#define SENSOR_RECHTS_HINTEN_PIN 6
-#define SENSOR_MITTE_VORNE_PIN 7
-#define SENSOR_MITTE_HINTEN_PIN 8
+  #define SENSOR_LINKS_VORNE_PIN 3
+  #define SENSOR_LINKS_HINTEN_PIN 4
+  #define SENSOR_RECHTS_VORNE_PIN 5
+  #define SENSOR_RECHTS_HINTEN_PIN 6
+  #define SENSOR_MITTE_VORNE_PIN 7
+  #define SENSOR_MITTE_HINTEN_PIN 8
 
-#define LINKS_VORNE_STOP 0b11001100
-#define LINKS_HINTEN_STOP 0b11000011
-#define RECHTS_VORNE_STOP 0b00111100
-#define RECHTS_HINTEN_STOP 0b00110011
-#define MITTE_VORNE_STOP 0b11111100
-#define MITTE_HINTEN_STOP 0b11110011
+  #define LINKS_VORNE_STOP 0b11001100
+  #define LINKS_HINTEN_STOP 0b11000011
+  #define RECHTS_VORNE_STOP 0b00111100
+  #define RECHTS_HINTEN_STOP 0b00110011
+  #define MITTE_VORNE_STOP 0b11111100
+  #define MITTE_HINTEN_STOP 0b11110011 */
 
 int wmotor = 0;
 int incomingByte = 0;
 int zielGeschwMotor [2];
-int momentanGeschwMotor[2];
-int startGeschwMotor [2];
-int x [2];
-int xMax = 100;
+double momentanGeschwMotor[2];
 bool manuellInterrupt = false;
-
+long letzterBefehlZeit = 0;
+float tau = 0.01;
 
 void setup() {
   Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), interrupt, RISING);
+  // attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), interrupt, RISING);
   pinMode(MOTOR_0_PIN, OUTPUT);
   pinMode(MOTOR_1_PIN, OUTPUT);
   pinMode(MOTOR_DIR_0_PIN, OUTPUT);
   pinMode(MOTOR_DIR_1_PIN, OUTPUT);
-  analogWriteFreq(333);
+ // analogWriteFreq(333);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   readSerial();
   geschwMotor0();
   geschwMotor1();
-  if (digitalRead(INTERRUPT_MANUELL_PIN) == HIGH) {
-    manuellInterrupt = true;
-    interrupt();
+  if (millis() - letzterBefehlZeit > 5000) {
+    zielGeschwMotor[0] = 0;
+    zielGeschwMotor[1] = 0;
   }
-  serialPrintGeschw ();
+  /* geschwMotor0();
+    geschwMotor1();
+    if (digitalRead(INTERRUPT_MANUELL_PIN) == HIGH) {
+     manuellInterrupt = true;
+     interrupt();
+    }
+    serialPrintGeschw ();*/
 }
 
 //-------------------------------------------------------------------------------------------------------------------------------------
@@ -57,21 +62,16 @@ void loop() {
 void readSerial() {
   if (Serial.available() > 0 ) {
     incomingByte  = Serial.read();
-    if (incomingByte > 127) {
-      incomingByte -= 128;
-    }
     if (wmotor == 0) {
-      zielGeschwMotor [0] = incomingByte * 4;
-      Serial.write(momentanGeschwMotor [0]);
-      startGeschwMotor [0] = momentanGeschwMotor [0];
-      x[0] = 0;
+      digitalWrite(13, HIGH);
+      zielGeschwMotor [0] = *(int8_t*)&incomingByte ;
       wmotor = 1;
+      letzterBefehlZeit = millis();
+
     } else {
-      zielGeschwMotor [1] = incomingByte * 4;
-      Serial.write(momentanGeschwMotor [1]);
-      startGeschwMotor [1] = momentanGeschwMotor [1];
-      x[1] = 0;
+      zielGeschwMotor [1] = *(int8_t*)&incomingByte;
       wmotor = 0;
+      letzterBefehlZeit = millis();
     }
   }
 }
@@ -79,44 +79,43 @@ void readSerial() {
 //-------------------------------------------------------------------------------------------------------------------------------------
 
 void geschwMotor0() {
-  if (momentanGeschwMotor [0] != momentanGeschwMotor [0]) {
-    if (abs(zielGeschwMotor [0] - momentanGeschwMotor [0]) < 5) {
-      momentanGeschwMotor [0] = zielGeschwMotor [0];
-    }
-    else {
-      momentanGeschwMotor [0] = linear(startGeschwMotor [0], zielGeschwMotor [0], 1, x[0]);
-    }
-    if (momentanGeschwMotor [0] < 0) {
-      digitalWrite(MOTOR_DIR_1_PIN, LOW);
-    }
-    else {
-      digitalWrite(MOTOR_DIR_1_PIN, HIGH);
-    }
-    analogWrite(MOTOR_0_PIN, abs(momentanGeschwMotor [0]));
+  if (abs(zielGeschwMotor[0] - momentanGeschwMotor[0]) < 4) {
+    momentanGeschwMotor [0] = zielGeschwMotor [0];
   }
+  else {
+    momentanGeschwMotor[0] += (int) ((zielGeschwMotor[0]-momentanGeschwMotor[0]) * tau * 1/(1+exp(0.02 * (abs(zielGeschwMotor[0]-momentanGeschwMotor[0])-100))));
+  }
+
+  if (momentanGeschwMotor [0] < 0) {
+    digitalWrite(MOTOR_DIR_0_PIN, LOW);
+  }
+  else {
+    digitalWrite(MOTOR_DIR_0_PIN, HIGH);
+  }
+  analogWrite(MOTOR_0_PIN, abs(momentanGeschwMotor [0]));
+
 }
 
 void geschwMotor1() {
-  if (momentanGeschwMotor [1] != momentanGeschwMotor [1]) {
-    if (abs(zielGeschwMotor [1] - momentanGeschwMotor [1]) < 5) {
-      momentanGeschwMotor [1] = zielGeschwMotor [1];
-    }
-    else {
-      momentanGeschwMotor [1] = linear(startGeschwMotor [1], zielGeschwMotor [1], 2, x[1]);
-    }
-    if (momentanGeschwMotor [1] < 0) {
-      digitalWrite(MOTOR_DIR_1_PIN, LOW);
-    }
-    else {
-      digitalWrite(MOTOR_DIR_1_PIN, HIGH);
-    }
-    analogWrite(MOTOR_1_PIN, abs(momentanGeschwMotor [1]));
+  if (abs(zielGeschwMotor[1] - momentanGeschwMotor[1]) < 4) {
+    momentanGeschwMotor [1] = zielGeschwMotor [1];
   }
+  else {
+    momentanGeschwMotor[1] += (int) ((zielGeschwMotor[1]-momentanGeschwMotor[1]) * tau * 1/(1+exp(0.02 * (abs(zielGeschwMotor[1]-momentanGeschwMotor[1])-100))));
+  }
+
+  if (momentanGeschwMotor [1] < 0) {
+    digitalWrite(MOTOR_DIR_1_PIN, LOW);
+  }
+  else {
+    digitalWrite(MOTOR_DIR_1_PIN, HIGH);
+  }
+  analogWrite(MOTOR_1_PIN, abs(momentanGeschwMotor [1]));
 }
 
 //-------------------------------------------------------------------------------------------------------------------------------------
 
-void interrupt() {
+/*void interrupt() {
   momentanGeschwMotor [0] = 0;
   momentanGeschwMotor [1] = 0;
   analogWrite(MOTOR_0_PIN, momentanGeschwMotor [0]);
@@ -128,9 +127,9 @@ void interrupt() {
     manuellInterrupt = false;
   }
   delay(5000);
-}
+  }
 
-void sendinterrupt() {
+  void sendinterrupt() {
   byte woStop = 0b00000000 ;
   if (digitalRead(SENSOR_LINKS_VORNE_PIN)) {
     woStop = LINKS_VORNE_STOP;
@@ -151,81 +150,7 @@ void sendinterrupt() {
     woStop = MITTE_HINTEN_STOP;
   }
   Serial.write(woStop);
-}
+  }
+*/
 
 //-------------------------------------------------------------------------------------------------------------------------------------
-
-void serialPrintGeschw() {
-  Serial.print("ZielGeschw ");
-  Serial.print(zielGeschwMotor[0]);
-  Serial.print(" ");
-  Serial.print(zielGeschwMotor[1]);
-  Serial.print(" Zeit ");
-  Serial.print(x[0]);
-  Serial.print(" ");
-  Serial.print(x[1]);
-  Serial.print(" Geschwindigkeit ");
-  Serial.print(momentanGeschwMotor[0]);
-  Serial.print(" ");
-  Serial.println(momentanGeschwMotor[1]);
-}
-
-//-------------------------------------------------------------------------------------------------------------------------------------
-
-int linear (int startGeschw, int zielGeschw, int motor, int xf) {
-  int endGeschw;
-  int m = (zielGeschw - startGeschw) / xMax;
-  endGeschw = m * xf + startGeschw;
-  if (motor == 1) {
-    x [0] = x [0] + 1;
-  }
-  else if (motor == 2) {
-    x [1] = x [1] + 1;
-  }
-  delay(10);
-  return endGeschw;
-}
-
-int quadratisch (int startGeschw, int zielGeschw, int motor, int xf) {
-  int endGeschw;
-  double m = (double) (zielGeschw - startGeschw) / (double) (xMax * xMax);
-  endGeschw = (int) (m * (xf * xf) + startGeschw);
-  if (motor == 1) {
-    x [0] = x [0] + 1;
-  }
-  else if (motor == 2) {
-    x [1] = x [1] + 1;
-  }
-  delay(10);
-  return endGeschw;
-}
-
-int wurzel (int startGeschw, int zielGeschw, int motor, int xf) {
-  int endGeschw;
-  int m = (zielGeschw - startGeschw) / sqrt(xMax);
-  endGeschw = m * sqrt(xf) + startGeschw;
-  if (motor == 1) {
-    x [0] = x [0] + 1;
-  }
-  else if (motor == 2) {
-    x [1] = x [1] + 1;
-  }
-  delay(10);
-  return endGeschw;
-}
-
-int logistisch (int startGeschw, int zielGeschw, int motor, int xf) {
-  int endGeschw;
-  int f0 = 1;
-  int g = zielGeschw + 1 - startGeschw + 1;
-  double m = ((0 - log(((g / (double)(zielGeschw - startGeschw + 1)) - 1) / ((g / (double)f0) - 1))) / (double) (g * xMax));
-  endGeschw = (int) ((g / (1 + pow(3.0, ((0 - m) * g * xf * (g / f0 - 1)))) + startGeschw - 1));
-  if (motor == 1) {
-    x [0] = x [0] + 1;
-  }
-  else if (motor == 2) {
-    x [1] = x [1] + 1;
-  }
-  delay(10);
-  return endGeschw;
-}
